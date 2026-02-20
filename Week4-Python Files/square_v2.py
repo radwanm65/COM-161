4194e02771cb95e7be94ad77681481a5c6a3224b
Dr Marwan Radwan <67072159+radwanm65@users.noreply.github.com>
2026-02-20 15:47:02 +0000

    Add files via upload

---
+++ Week4-Python Files/square_v2.py
@@ -0,0 +1,15 @@
+
+# this program draws a square of side 100
+
+#import turtle
+import turtle
+
+# draw a line of length 100
+# then turn left thru 90 degrees
+# repeat this process 4 times to
+# draw the four sides of the square
+
+for i in range(8):
+    turtle.forward(100)
+    turtle.left(45)
+#end of for loop
---
+++ Week4-Python Files/squares.py
@@ -0,0 +1,7 @@
+print('number\tSquare')
+print('---------------')
+
+for number in range (20,0,-1):
+    square = number**2
+    print(number, '\t', square)
+    
